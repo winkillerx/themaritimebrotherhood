54fba47f7a9044123fd9a3443acd232b275fb864
winkillerx <the_hump_god@hotmail.com>
2026-02-11 22:03:03 -0500

    Delete api/popular-movies.js

--- api/popular-movies.js
+++
@@ -1,7 +0,0 @@
-// api/popular-movies.js
-import popular from "./popular.js";
-
-export default function handler(req, res) {
-  req.query = { ...(req.query || {}), media: "movie" };
-  return popular(req, res);
-}
