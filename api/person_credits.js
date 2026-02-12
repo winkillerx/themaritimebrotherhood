a5cbb3af0a0fce9aa0cd778ee6f9c0e1ea3a5490
winkillerx <the_hump_god@hotmail.com>
2026-02-11 22:00:01 -0500

    Delete api/person_credits.js

--- api/person_credits.js
+++
@@ -1,9 +0,0 @@
-import { tmdbFetch } from './_tmdb.js';
-
-export default async function handler(req, res) {
-  const id = req.query.id;
-  if (!id) return res.status(400).json({ error: 'Missing id' });
-
-  const data = await tmdbFetch(`/person/${id}/combined_credits`);
-  res.status(200).json(data);
-}
