4f7a98db31ff06e271202ceda246708582a589de
winkillerx <the_hump_god@hotmail.com>
2026-02-22 15:38:56 -0400

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,8 +1,9 @@
-// analytics.js  (GitHub ONLY)
 (function () {
   fetch("https://analytics.filmmatrix.net/track/", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify({
       page: window.location.pathname,
       title: document.title,
